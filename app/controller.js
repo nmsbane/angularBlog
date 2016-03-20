1980092d1ffdffb99e4efde875e5e8b03f6f66e4
nmsbane <madhusudhan638@gmail.com>
2016-03-20 17:52:12 +0530

    created a first directive navigationbar and the navigation css is present in partials directory

--- app/controller.js
+++ app/controller.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['app.directives'])
               .controller('PostsController', ['$scope', '$http', function($scope, $http){
                   $http.get('data/posts.json').success(function(data){
                     $scope.posts = data;
